7e704a2d7a2838542135a175edd8e3ccd901eb12
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2023-08-24 17:26:33 +0200

    task one done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+let picArray = [];
+galleryItems.forEach((galleryItem) => {
+  const liItem = document.createElement("li");
+
+  const innerString = `<a class="gallery__item" href="${galleryItem.original}">
+<img
+  class="gallery__image"
+  src="${galleryItem.preview}"
+  alt="${galleryItem.description}"
+/>
+</a>`;
+  liItem.insertAdjacentHTML("beforeend", innerString);
+
+  picArray.push(liItem);
+});
+
+gallery.append(...picArray);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
