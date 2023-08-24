4e21c09545e8cd21fdde8b0289ed185cb07ccf9c
Zuzanna Maciejczyk <zuzannamaciejczyk@gmail.com>
2023-08-24 17:29:10 +0200

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  }, 1000)
+
+);
+player
+  .setCurrentTime(Number(localStorage.getItem(`videoplayer-current-time`)))
+  .then(seconds => console.log(`seconds: ${seconds} = the pause time`))
+
+
+const storedTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(storedTime);
 
